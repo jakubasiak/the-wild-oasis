8e504463d8964c32a7a4d78ba7aebf5654d6f4f2
Jakub Kubasiak <kubakubasiak@o2.pl>
2025-06-26 16:02:03 +0200

    styled components basics

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,39 @@
 import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
-`;
 
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
-  cursor: pointer;
-  margin: 20px;
-`;
 
-const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.8rem 1.2rem;
-`;
-
-const StyledApp = styled.div`
-background-color: orangered;
+const StyledApp = styled.main`
 padding: 20px;
 `;
 
 export default function App() {
   return (
-    <StyledApp>
-      <H1>The Wild Oasis</H1>
-      <Button onClick={() => alert('Check in')}>Check in</Button>
-      <Button onClick={() => alert('Check out')}>Check in</Button>
-      <Input type="number" placeholder="Number of guests" />
-    </StyledApp>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row>
+          <Row type="horizontal">
+            <Heading as='h1'>The Wild Oasis</Heading>
+            <div>
+              <Heading as='h2'>Check in and out</Heading>
+              <Button onClick={() => alert('Check in')}>Check in</Button>
+              <Button variation="secondary" size="small" onClick={() => alert('Check out')}>Check in</Button>
+            </div>
+          </Row>
+          <Row>
+            <Heading as='h3'>Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
+      </StyledApp>
+    </>
   );
 }
