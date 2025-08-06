128b9fba6939f7a41e3f5caa14cb9ac0beb42000
Jakub Kubasiak <kubakubasiak@o2.pl>
2025-08-06 14:37:13 +0200

    add dashboard component

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,9 @@
 import styled from 'styled-components';
+import Stats from './Stats';
+import Spinner from '../../ui/Spinner';
+import { useRecentBookings } from './useRecentBookings';
+import { useRecentStays } from './useRecentStays';
+import { useCabins } from '../cabins/useCabins';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +11,22 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  const { isLoading: isLoadingBookings, bookings } = useRecentBookings();
+  const { isLoading: isLoadingStays, stays, confirmedStays, numDays } = useRecentStays();
+  const { isLoading: isLoadingCabins, cabins } = useCabins();
+
+  if (isLoadingBookings || isLoadingStays || isLoadingCabins) return <Spinner />;
+
+  return (
+    <StyledDashboardLayout>
+      <Stats bookings={bookings} confirmedStays={confirmedStays} numDays={numDays} cabinCount={cabins.length} />
+      <div>Today&apos;s activity</div>
+      <div>Chart stay durations</div>
+      <div>Chart sales</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
